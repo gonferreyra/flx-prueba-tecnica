0d5899fc3920b34135e8041d756ffd35f822f1f9
Gonzalo Ferreyra <gonferreyra@gmail.com>
2024-07-17 11:57:26 -0300

    useForm custom hook created

--- client/src/lib/hooks.ts
+++ client/src/lib/hooks.ts
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { UsersContext } from '../contexts/UsersContextProvider';
 
 export function useUsersContext() {
@@ -12,3 +12,41 @@ export function useUsersContext() {
 
   return context;
 }
+
+// ----------------------------------------------------------------------
+
+export const useForm = <T>(initialState: T) => {
+  const [formState, setFormState] = useState<T>(initialState);
+
+  const onInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+
+  const onSelectChange = (value: string) => {
+    const event = {
+      target: {
+        name: 'status',
+        value,
+      },
+    } as React.ChangeEvent<HTMLInputElement>;
+
+    onInputChange(event);
+  };
+
+  const onResetForm = () => {
+    setFormState(initialState);
+  };
+
+  return {
+    ...formState,
+    setFormState,
+    formState,
+    onInputChange,
+    onResetForm,
+    onSelectChange,
+  };
+};
