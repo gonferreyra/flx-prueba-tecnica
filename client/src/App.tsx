cd9875a4864dbaa64901e129ac675c5aae25c4e6
Gonzalo Ferreyra <gonferreyra@gmail.com>
2024-07-18 19:09:15 -0300

    added context to app and providers to main

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,47 +2,21 @@ import { Layout } from 'antd';
 import HeaderComponent from './components/Header/Header';
 import UsersControlPanel from './components/UsersControlPanel/UsersControlPanel';
 import UsersTable from './components/UsersTable/UsersTable';
-import UsersContextProvider from './contexts/UsersContextProvider';
+
+import styles from './App.module.css';
 
 const { Content } = Layout;
 
-import styles from './App.module.css';
-import { useState } from 'react';
-
 function App() {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [mode, setMode] = useState('add');
-
-  const handleOpenModal = (mode: string) => {
-    setMode(mode);
-    setIsModalOpen(true);
-  };
-
-  const handleCloseModal = () => {
-    setIsModalOpen(false);
-  };
-
   return (
     <>
       <Layout>
         <HeaderComponent />
 
         <Content className={styles.main}>
-          <UsersContextProvider>
-            <UsersControlPanel
-              isModalOpen={isModalOpen}
-              mode={mode}
-              handleOpenModal={handleOpenModal}
-              handleCloseModal={handleCloseModal}
-            />
+          <UsersControlPanel />
 
-            <UsersTable
-              isModalOpen={isModalOpen}
-              mode={mode}
-              handleOpenModal={handleOpenModal}
-              handleCloseModal={handleCloseModal}
-            />
-          </UsersContextProvider>
+          <UsersTable />
         </Content>
       </Layout>
     </>
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,9 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import ModalContextProvider from './contexts/ModalContextProvider.tsx';
+import SearchTextContextProvider from './contexts/SearchTextContextProvider.tsx';
+import UsersContextProvider from './contexts/UsersContextProvider.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <SearchTextContextProvider>
+      <UsersContextProvider>
+        <ModalContextProvider>
+          <App />
+        </ModalContextProvider>
+      </UsersContextProvider>
+    </SearchTextContextProvider>
   </React.StrictMode>
 );
